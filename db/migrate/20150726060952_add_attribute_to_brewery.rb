f7927079cc58fd37a1ba5f2956447d1617d33ca3
Matt <matthellwinkel@gmail.com>
2015-08-09 16:27:31 -0600

    remove migrations

--- db/migrate/20150726060952_add_attribute_to_brewery.rb
+++
@@ -1,5 +0,0 @@
-class AddAttributeToBrewery < ActiveRecord::Migration
-  def change
-    add_column :breweries, :name, :string
-  end
-end
--- db/migrate/20150726061120_add_attribute_to_brewery2.rb
+++
@@ -1,5 +0,0 @@
-class AddAttributeToBrewery2 < ActiveRecord::Migration
-  def change
-    add_column :breweries, :city, :string
-  end
-end
