844f91208071eeeccffdeade00cddd84bf54927b
Matt Hellwinkel <matthellwinkel@gmail.com>
2015-08-14 15:33:05 -0600

    ranking controller updates

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,9 +1,16 @@
 class RankingsController < ApplicationController
   def index
-    sql = "SELECT AVG(r.value), b.name FROM ratings r, brews b
+    sql = "SELECT AVG(r.value) AS avg, b.name AS bname, br.name AS brname FROM ratings r, brews b, breweries br
            WHERE r.brew_id = b.id
-           GROUP BY r.brew_id, b.name
-           ORDER BY AVG(r.value) DESC"
+           AND b.brewery_id = br.id
+           GROUP BY r.brew_id, b.name, br.name
+           ORDER BY AVG(r.value) DESC
+           LIMIT 10"
     @rankings  = ActiveRecord::Base.connection.execute(sql)
+
+    sql2 = "SELECT AVG(r.value), br.name AS avg FROM ratings r, breweries br
+            ORDER BY AVG(r.value), br.name DESC
+            LIMIT 5"
+    @breweries = ActiveRecord::Base.connection.execute(sql2)
   end
 end
